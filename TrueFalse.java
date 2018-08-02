130c4b8deb4f5aa74ed72569372ab0b4926f5e21
bonjih <37001472+bonjih@users.noreply.github.com>
2018-08-02 17:13:17 +1000

    first GeoQuiz upload

---
+++ TrueFalse.java
@@ -0,0 +1,31 @@
+package com.bignerdranch.android.geoquiz;
+
+public class TrueFalse {
+
+    //declare
+    private int mQuestion;
+    private boolean mTrueQuestion;
+
+    // constructor
+    public TrueFalse(int question, boolean trueQuestion) {
+        mQuestion = question;
+        mTrueQuestion = trueQuestion;
+    }
+
+    public int getQuestion() {
+        return mQuestion;
+    }
+
+    private void setQuestion(int question) {
+        mQuestion = question;
+    }
+
+    public boolean isTrueQuestion() {
+        return mTrueQuestion;
+    }
+
+    public void setTrueQuestion(boolean trueQuestion) {
+        mTrueQuestion = trueQuestion;
+    }
+
+}
