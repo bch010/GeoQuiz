6ffbae73715125691528a4800fa4393e96d00e04
bonjih <37001472+bonjih@users.noreply.github.com>
2018-08-02 18:39:55 +1000

    first GeoQuiz upload

---
+++ QuizActivity.java
@@ -0,0 +1,127 @@
+package com.bignerdranch.android.geoquiz;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class QuizActivity extends AppCompatActivity {
+
+    //declare buttons
+    private Button mTrueButton;
+    private Button mFalseButton;
+    private ImageButton mNextButton;
+    private ImageButton mPrevButton;
+    private TextView mQuestionTextView;
+
+    // True/False array
+    private TrueFalse[] mQuestionBank = new TrueFalse[]{
+            new TrueFalse(R.string.question_oceans, true),
+            new TrueFalse(R.string.question_mideast, false),
+            new TrueFalse(R.string.question_africa, false),
+            new TrueFalse(R.string.question_americas, true),
+            new TrueFalse(R.string.question_asia, true),
+    };
+    private int mCurrentIndex = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+
+        //controller part
+        // Link question from bank to view
+        mQuestionTextView = findViewById(R.id.question_text_view);
+
+        onTrueButtonClick();
+        onFalseButtonClick();
+        onNextButtonClick();
+        onPrevButtonClick();
+        updateQuestion(); // first time called add 1 to set text
+    }
+
+    /**
+     * When true button is pressed, returns correct answer
+     */
+    protected void onTrueButtonClick() {
+        mTrueButton = findViewById(R.id.true_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                checkAnswer(true);
+            }
+        });
+    }
+
+    /**
+     * When false button is pressed, returns incorrect answer
+     */
+    protected void onFalseButtonClick() {
+        mFalseButton = findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAnswer(false);
+            }
+        });
+    }
+
+    /**
+     * When next button is pressed, returns next questions
+     */
+    protected void onNextButtonClick() {
+        mNextButton = findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                updateQuestion(); // called as part of the onCreate method, sets a default value to Textview
+            }
+        });
+    }
+
+    /**
+     * When previous button is pressed, returns next questions
+     */
+    protected void onPrevButtonClick() {
+        mPrevButton = findViewById(R.id.prev_button);
+        mPrevButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex + mQuestionBank.length - 1) % mQuestionBank.length;
+                updateQuestion(); // called as part of the onCreate method, sets a default value to Textview
+            }
+        });
+    }
+
+    /**
+     * Updates questions
+     */
+    private void updateQuestion() {
+
+        int question = mQuestionBank[mCurrentIndex].getQuestion();
+        mQuestionTextView.setText(question);
+    }
+
+    /**
+     * Checks if selected answer is correct
+     */
+    private void checkAnswer(boolean userPressedTrue) {
+        boolean answerIsTrue = mQuestionBank[mCurrentIndex].isTrueQuestion();
+        int messageResId = 0;
+        if (userPressedTrue == answerIsTrue) {
+            messageResId = R.string.correct_toast;
+        } else {
+            messageResId = R.string.incorrect_toast;
+        }
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
+                .show();
+    }
+}
+
