326a2f10d8f397f9f623cb2c355dd6c0f1125e4c
bonjih <37001472+bonjih@users.noreply.github.com>
2018-08-05 07:32:02 +1000

    Chapter 6 cheat additions TODO // Qeoquiz to cheat activity does nto work correctly. Does not act as seperate activity. Is the cheat Manifest addition correct?

--- QuizActivity.java
+++ QuizActivity.java
@@ -1,5 +1,6 @@
 package com.bignerdranch.android.geoquiz;
 
+import android.content.Intent; // to interact with components from the same applications as well as with components contributed by other applications
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -13,10 +14,12 @@ public class QuizActivity extends AppCompatActivity {
     //declare buttons
     private Button mTrueButton;
     private Button mFalseButton;
+    private Button mCheatButton;
     private ImageButton mNextButton;
     private ImageButton mPrevButton;
+
+    private boolean mIsCheater;
     private TextView mQuestionTextView;
-    private TextView textView_nextQuestion;
 
     // True/False array
     private TrueFalse[] mQuestionBank = new TrueFalse[]{
@@ -41,6 +44,7 @@ public class QuizActivity extends AppCompatActivity {
 
         mQuestionTextView = findViewById(R.id.question_text_view);
         mQuestionTextView.setOnClickListener(new View.OnClickListener() {
+
             // When TextView is pressed, user sees next question
             @Override
             public void onClick(View v) {
@@ -55,6 +59,7 @@ public class QuizActivity extends AppCompatActivity {
         onFalseButtonClick();
         onNextButtonClick();
         onPrevButtonClick();
+        onCheatButtonClick();
 
         updateQuestion(); // first time called add 1 to set text
     }
@@ -103,6 +108,7 @@ public class QuizActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                mIsCheater = false;
                 updateQuestion(); // called as part of the onCreate method, sets a default value to Textview
             }
         });
@@ -124,6 +130,24 @@ public class QuizActivity extends AppCompatActivity {
     }
 
     /**
+     * When clicked, displays answer
+     */
+    protected void onCheatButtonClick() {
+        mCheatButton = findViewById(R.id.cheat_button);
+        mCheatButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(QuizActivity.this, CheatActivity.class);
+                boolean answerIsTrue = mQuestionBank[mCurrentIndex].isTrueQuestion();
+                i.putExtra(CheatActivity.EXTRA_ANSWER_IS_TRUE, answerIsTrue); // intent extra
+                startActivityForResult(i, 0);
+            }
+        });
+        updateQuestion();
+    }
+
+    /**
      * Updates questions
      */
     private void updateQuestion() {
@@ -135,16 +159,31 @@ public class QuizActivity extends AppCompatActivity {
      * Checks if selected answer is correct
      */
     private void checkAnswer(boolean userPressedTrue) {
+
         boolean answerIsTrue = mQuestionBank[mCurrentIndex].isTrueQuestion();
         int messageResId = 0;
-        if (userPressedTrue == answerIsTrue) {
-            messageResId = R.string.correct_toast;
+
+        if (mIsCheater) {
+            messageResId = R.string.judgment_toast;
         } else {
-            messageResId = R.string.incorrect_toast;
+            if (userPressedTrue == answerIsTrue) {
+                messageResId = R.string.correct_toast;
+            } else {
+                messageResId = R.string.incorrect_toast;
+            }
         }
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
     }
 
+    // Check if user cheated
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (data == null) {
+            return;
+        }
+        mIsCheater = data.getBooleanExtra(CheatActivity.EXTRA_ANSWER_SHOWN, false);
+    }
 
 }
 
